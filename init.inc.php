19a21cff9d3352bfd569530e8970f7daaccfe6a5
rwiscart <rwiscart@hotmail.fr>
2016-12-07 14:13:08 +0100

    Update init.inc.php

--- init.inc.php
+++ init.inc.php
@@ -1,8 +1,7 @@
 <?php
 // Connexion à la BDD site :
 
-$pdo = new PDO('mysql:host=localhost; dbname=save_track', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-
+$pdo = new PDO('mysql:host=212.47.252.97;port=3306;dbname=track_save', 'root', 'b395dd635afe9f2854bf64dc4269dfeb', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 // création ou ouverture d'une session:
 
 session_start();
